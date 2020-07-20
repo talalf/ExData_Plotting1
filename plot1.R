78970f8bb2dfa614529beddc2c320292a16e7a46
talalf <talal.fazmin1@gmail.com>
2020-07-20 13:45:16 +0100

    plot1.R done - now output file is of correct width and height

--- plot1.R
+++ plot1.R
@@ -34,5 +34,5 @@ hpc <- hpc %>% select(day, everything())
 with(hpc, hist(Global_active_power, main = "Global Active Power", col = "red",
                xlab = "Global Active Power (kilowatts)", ylab = "Frequency"))
 
-dev.copy(png, file = "plot1.png")
+dev.copy(png, file = "plot1.png", width = 480, height = 480)
 dev.off()
